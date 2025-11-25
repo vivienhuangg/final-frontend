2ff8fba9d70d07d28d8be654ec77dc7b9e44e9d7
“Amalia <amaliat@mit.edu>
2025-11-24 23:01:45 -0500

    Made edits for dpeloyment.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
